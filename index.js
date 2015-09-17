1d2ed5c7abb34c9bd22b4ab6594b8a7d45da3569
william-bishop <admin@the402.net>
2015-09-17 16:01:17 -0700

    Using heroku-supplied port

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + '/webroot'));
 
@@ -23,6 +24,6 @@ io.on('connection', function (socket) {
     });
 });
 
-http.listen(3000, function () {
-    console.log('listening on *:3000');
+http.listen(port, function () {
+    console.log('listening on *:' + port);
 });
