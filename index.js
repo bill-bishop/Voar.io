9bfdbaec6ed4a9ebf15a40fd7d04c70c970cce1a
william-bishop <admin@the402.net>
2015-09-22 19:46:31 -0700

    deleting server-side phaser… not a valid option

--- index.js
+++ index.js
@@ -7,54 +7,14 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 5000;
-var gameEvents = require('./game/game-events');
 
 app.use(express.static(__dirname + '/webroot'));
-
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/webroot/index.html');
-});
-
-
+app.use(express.static(__dirname + '/game'));
 
 var shipMap = {};
 
 io.on('connection', function (socket) {
     console.log('player connected');
-
-    for(var id in shipMap) {
-        socket.emit('ship', shipMap[id]);
-    }
-    socket.on('thrust', function (ship) {
-        socket.broadcast.emit('thrust', ship);
-    });
-
-    socket.on('ship', function (options) {
-        shipMap[options.shipId] = options;
-        socket.broadcast.emit('ship', options);
-
-        socket.on('disconnect', function () {
-            killShip(options);
-        });
-    });
-
-    socket.on('update', function (options) {
-        if(shipMap[options.shipId])
-            shipMap[options.shipId] = options;
-    });
-
-    setInterval(function () {
-        io.emit('update', shipMap);
-    }, 1000);
-
-    socket.on('kill', killShip);
-
-    function killShip (options) {
-        if(shipMap[options.shipId]) {
-            delete shipMap[options.shipId];
-            socket.broadcast.emit('kill', options);
-        }
-    }
 });
 
 http.listen(port, function () {
